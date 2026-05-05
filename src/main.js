82597c778a4068c38cf14daa1bc6b8429bd890e0
ouno1054 <ouno1054@ounodeMacBook-Air.local>
2026-05-06 01:39:47 +0800

    refactor: rewrite main.js with vanilla JS, remove jQuery dependency

--- src/main.js
+++ src/main.js
@@ -6,21 +6,16 @@ import './js/showdown-plugins/showdown-footnote.js'
 
 import './js/google-code-prettify/run_prettify.js'
 
-var CodeTheme = require('./js/theme/code-theme.js')
-var PageTheme = require('./js/theme/page-theme.js')
+import initCodeTheme from './js/theme/code-theme.js'
+import initPageTheme from './js/theme/page-theme.js'
 
-var $ = require('./js/jquery-3.1.1.js')
 var showdown = require('./js/showdown.js')
 var Clipboard = require('./js/clipboard.min.js')
 
-var kv = location.href.split('?')[1]
-kv = (kv && kv.split('&')) || []
 var params = {}
-$.each(kv, function (index, item) {
-  var m = (item || '').split('=')
-  if (m && m[0] && m[1]) {
-    params[m[0]] = m[1]
-  }
+var searchParams = new URLSearchParams(window.location.search)
+searchParams.forEach(function (value, key) {
+  params[key] = value
 })
 
 var converter = new showdown.Converter({
@@ -29,96 +24,92 @@ var converter = new showdown.Converter({
 })
 
 function showSnackbar() {
-  var $snackbar = $('#snackbar')
-  $snackbar.addClass('show')
-  setTimeout(() => {
-    $snackbar.removeClass('show')
+  var snackbar = document.getElementById('snackbar')
+  snackbar.classList.add('show')
+  setTimeout(function () {
+    snackbar.classList.remove('show')
   }, 3000)
 }
 
-var OnlineMarkdown = {
-  currentState: 'edit',
-  init: function () {
-    var self = this
-    self
-      .load()
-      .then(function () {
-        self.start()
-      })
-      .fail(function () {
-        self.start()
-      })
-  },
-  start: function () {
-    this.bindEvt()
-    this.updateOutput()
-    new CodeTheme()
-    new PageTheme()
-  },
-  load: function () {
-    return $.ajax({
-      type: 'GET',
-      url: params.path || './demo.md',
-      dateType: 'text',
-      data: {
-        _t: new Date() * 1
-      },
-      timeout: 2000
-    }).then(function (data) {
-      $('#input').val(data)
-    })
-  },
-  bindEvt: function () {
-    $('#input').on('input keydown paste', this.updateOutput)
+function updateOutput() {
+  var input = document.getElementById('input')
+  var outputWrapper = document.querySelector('#output .wrapper')
+  var val = converter.makeHtml(input.value)
+  outputWrapper.innerHTML = val
+  PR.prettyPrint()
 
-    var clipboard = new Clipboard('.copy-button')
-    clipboard.on('success', function () {
-      showSnackbar()
-    })
-    clipboard.on('error', function (e) {
-      console.log(e)
-    })
-
-    var hasInputScroll = false
-    var hasOutputScroll = false
-    $('#input').on('scroll', (event) => {
-      if (hasInputScroll) {
-        hasInputScroll = false
-      } else {
-        var $output = $('#outputCtt')
-        var outputScrollHeight =
-          (event.currentTarget.scrollTop / event.currentTarget.scrollHeight) *
-          $output.prop('scrollHeight')
-        hasOutputScroll = true
-        $output.scrollTop(outputScrollHeight)
-      }
-    })
-    $('#outputCtt').on('scroll', (event) => {
-      if (hasOutputScroll) {
-        hasOutputScroll = false
-      } else {
-        var $input = $('#input')
-        var inputScrollHeight =
-          (event.currentTarget.scrollTop / event.currentTarget.scrollHeight) *
-          $input.prop('scrollHeight')
-        hasInputScroll = true
-        $input.scrollTop(inputScrollHeight)
-      }
-    })
-  },
-
-  updateOutput: function () {
-    var val = converter.makeHtml($('#input').val())
-    $('#output .wrapper').html(val)
-    PR.prettyPrint()
-    $('#outputCtt li').each(function () {
-      var content = $(this).html()
-      if (content.indexOf('<p>') === 0) {
-        content = content.substr(3, content.length - 7)
-      }
-      $(this).html('<span><span>' + content + '</span></span>')
-    })
+  var items = document.querySelectorAll('#outputCtt li')
+  for (var i = 0; i < items.length; i++) {
+    var content = items[i].innerHTML
+    if (content.indexOf('<p>') === 0) {
+      content = content.substr(3, content.length - 7)
+    }
+    items[i].innerHTML = '<span><span>' + content + '</span></span>'
   }
 }
 
-OnlineMarkdown.init()
+function loadDemo() {
+  var demoPath = params.path || './demo.md'
+  return fetch(demoPath + '?_t=' + Date.now())
+    .then(function (res) {
+      if (!res.ok) throw new Error('Failed to load ' + demoPath)
+      return res.text()
+    })
+    .then(function (text) {
+      document.getElementById('input').value = text
+    })
+}
+
+function bindEvents() {
+  var input = document.getElementById('input')
+
+  input.addEventListener('input', updateOutput)
+  input.addEventListener('keydown', updateOutput)
+  input.addEventListener('paste', updateOutput)
+
+  var clipboard = new Clipboard('.copy-button')
+  clipboard.on('success', function () {
+    showSnackbar()
+  })
+  clipboard.on('error', function (e) {
+    console.log(e)
+  })
+
+  // Sync scroll
+  var hasInputScroll = false
+  var hasOutputScroll = false
+
+  input.addEventListener('scroll', function () {
+    if (hasInputScroll) {
+      hasInputScroll = false
+    } else {
+      var outputCtt = document.getElementById('outputCtt')
+      var ratio = input.scrollTop / input.scrollHeight
+      hasOutputScroll = true
+      outputCtt.scrollTop = ratio * outputCtt.scrollHeight
+    }
+  })
+
+  document.getElementById('outputCtt').addEventListener('scroll', function () {
+    var outputCtt = this
+    if (hasOutputScroll) {
+      hasOutputScroll = false
+    } else {
+      var ratio = outputCtt.scrollTop / outputCtt.scrollHeight
+      hasInputScroll = true
+      input.scrollTop = ratio * input.scrollHeight
+    }
+  })
+}
+
+// Init
+loadDemo()
+  .catch(function () {
+    // If demo.md fails to load, continue anyway
+  })
+  .finally(function () {
+    bindEvents()
+    updateOutput()
+    initCodeTheme()
+    initPageTheme()
+  })
